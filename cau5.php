fdf6023d1cd21e7bd4d7861c333bf3c68ec9ff80
phamxuanphu278 <33858715+phamxuanphu278@users.noreply.github.com>
2018-10-05 20:39:11 +0700

    Add files via upload

--- cau5.php
+++ cau5.php
@@ -1,49 +1,59 @@
-<?php  
-	require 'index.php';
+<?php
+	include('./users.php');
+	$obj_users = new users();
+	$users = $obj_users ->getUsers();
+	var_dump($users);
+	die();
 ?>
-
-<!DOCTYPE html>
-<html>
+<!DOCTYPE!>
+<html lang="en">
 <head>
 	<title>
-		
+		Document
 	</title>
+	<link rel="stylesheet" type="text/css" href="./TaiNguyen/css/bootstrap.min">
+	<link rel="stylesheet" type="text/css" href="./TaiNguyen/fonts">
+	<link rel="stylesheet" type="text/css" href="./TaiNguyen/css/styles.css">
+	<style type="text/css">
+		.ourteam{
+			color: blue;
+			text-align: center;
+			font-size: 20px;
+			padding: 60px 0px;
+		}
+		.mds1 {
+			padding:30px 0px 0px 0px;
+		}
+		.mds2 {
+			padding: 40px 0px 0px 0px;
+		}
+		.mds3 {
+			padding: 0px 0px 0px 40px;
+
+		}
+	</style>
 </head>
 <body>
-
-	<br> <br>
-	<form>
-		<b> ID </b> <input type="txt" name="id" placeholder="Tìm kiếm ID">
-		<input type="submit" name="submit" id="submit" value="Tìm kiếm">
-	</form>
-		<?php var_dump(empty($_GET['id'])) ;
-		if (empty($_GET['id'])) echo "chưa nhập...";
-		else {
-			 $pet = $_GET['id'];
-			 $temp = 0;
-             $a = 0;
-             for ($i=0; $i < 5; $i++){ 
-                   if($users[$i]['id'] == $pet)
-                   {
-                        $temp++;
-                        $a = $i;
-                   }
-             }
-             if ($temp == 1)
-             {
-                echo "co ton tai user: "; 
-                echo $users[$a]['name'];
-                echo $users[$a]['email']['gmail'];
-                echo $users[$a]['email']['yahoo'];
-             }
-             else
-             {
-                echo "Khong ton tai user co ID = ";
-                echo $pet;
-             }
-		}
-			
-		
-        ?>
+	
+	<div class="container">
+		<div class="ourteam">  OUR TEAM </div>
+		<div class="row">
+			<div class="col-md-4"> 
+				<img src="public/images/1.png">
+				<a href="#"><br><p class="mds3"> ROBERT JOHNSON </p></a>
+			</div>
+			<div class="col-md-4"> 
+				<div class="mds1">
+					<img src="public/images/2.png">
+					<a href="#"><br><p class="mds3"> ROBERT JOHNSON </p> </a>
+				</div>
+			</div>
+			<div class="col-md-4"> 
+				<div class="mds2">
+					<img src="public/images/3.png">
+					<a href="#"><br><p class="mds3"> ROBERT JOHNSON </p></a>
+				</div>
+			</div>
+	</div>
 </body>
 </html>
---
+++ db.php
@@ -0,0 +1,25 @@
+<?php
+include('config.php');
+
+class db {
+	public static $connection = NULL;
+
+	public function __construct() {
+		if (!self::$connection)
+		{
+			self::$connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+		}
+	}
+	public function query ($sql) {
+		$data = mysqli_query(self::$connection, $sql);
+		return $data;
+	}
+	public function select($sql) {
+		$data = $this->query($sql);
+		$row = [];
+		while ($row = mysqli_fetch_assoc($data)) {
+			$row[] = $row ;
+		}
+		return $row;
+	}
+}
